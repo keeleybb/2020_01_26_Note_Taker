afa7874ec2940c7852d59bf370cb3106b1697ccf
kbb <54555434+keeleybb@users.noreply.github.com>
2020-01-25 08:30:52 -0700

    get post working

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -5,70 +5,79 @@ const fs = require("fs");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static(path.resolve('../../'))); //To make static css and js work
+
 //allowing express to use json
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
+//File Paths
 const filePath = (path.resolve('../../../db/db.json'));
-// var file2 = fs.readFile('../../../db/db.json')
-
-
 
 var obj = []; //Temp Holder
 
-//Routes
-app.get("/", function (req, res) {
-    res.sendFile(path.resolve('../../index.html'));
-})
-
-app.get("/notes", function (req, res) {
-    res.sendFile(path.resolve('../../notes.html'));
-})
-
 //API
 app.get("/api/notes", function (req, res) {
-    return res.sendFile(path.resolve('../../../db/db.json'));
+    // var dbFile = JSON.parse(require('fs').readFileSync('../../../db/db.json', 'utf8'));
+    console.log("Is API actually getting called?");
+    return res.json(JSON.parse(require('fs').readFileSync(filePath, 'utf8')));
 })
-
 
 
 app.post("/api/notes", function (req, res) {
     //How to push to an outside file with no name
     const newNotes = req.body;
     console.log(newNotes);
-    // obj.push(newNotes);
-    // var json = JSON.stringify(obj);
-    // console.log(req);
-    // filePath.push(JSON.stringify(newNotes));
 
     fs.readFile(filePath, 'utf8', function readFileCallback(err, data) {
         if (err) {
             console.log(err);
         } else {
-            obj = JSON.parse(data); //now it's an object
+            obj = JSON.parse(data); //now filePath is an object
             obj.push(newNotes); //add some data
-            json = JSON.stringify(obj); //convert it back to json
+            let json = JSON.stringify(obj); //convert it back to json
             fs.writeFile(filePath, json, 'utf8', function (err) {
                 if (err) throw err;
                 console.log('done!');
             });// write it back 
         }
     });
+    return res.json(JSON.parse(require('fs').readFileSync(filePath, 'utf8')));
 
-    // fs.appendFile((path.resolve('../../../db/db.json')), ', ' + JSON.stringify(newNotes) + ']', function (err) {
-    //     if (err) throw err;
-    //     console.log('done!');
-    //     // const filename = (path.resolve('../../../db/db.json'));
-    //     // filename.push(newNotes)
-    // })
-
-    //push newNotes to file
-    // console.log();
-    // return fs.readFile('../../../db/db.json', JSON.strigify(jsonData));
 })
+
+// app.delete("/api/notes/:id", function (req, res) {
+//Read the file, rewrite variable with data.. Iterate through it to find the ID, then pop it out , then rewrite the file and reload page
+//     var indexOfCouseInJson = filePath.map(function (item) { return item.id; }).indexOf(req.params.id); //find the index of :id
+//     if (indexOfCouseInJson === -1) {
+//         res.statusCode = 404;
+//         return res.send('Error 404: No quote found');
+//     }
+
+//     var result = json.splice(indexOfCouseInJson, 1);
+//     fs.writeFile(jsonFilePath, JSON.stringify(result), function (err) {
+//         if (err) throw err;
+//         res.json(true);
+//     });
+// }
+// );
+
+//Routes
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname + '/../../index.html'));
+})
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.resolve(__dirname + '/../../notes.html'));
+})
+
 
 // Starts the server to begin listening
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
+    // console.log(path.resolve('../../'));
+    // console.log(path.resolve(__dirname + '/../../notes.html'));
 })
+
+
